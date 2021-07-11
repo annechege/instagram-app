4d0a71a2238e5ef3fee0600090fdfb74e1b4036e
annechege <annechege780@gmail.com>
2021-07-11 22:34:31 +0300

    adding code to urls.py

--- Pictures/apps.py
+++ Pictures/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class PicturesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class PicsConfig(AppConfig):
     name = 'Pictures'
